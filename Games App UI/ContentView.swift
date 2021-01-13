d64715ed060c4b279b0b821981524843f11bf5e1
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-01-13 21:27:23 +0530

    Initial Commit

---
+++ Games App UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Games App UI
+//
+//  Created by Mahesh Prasad on 13/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Games App UI/Games_App_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Games_App_UIApp.swift
+//  Games App UI
+//
+//  Created by Mahesh Prasad on 13/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Games_App_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
